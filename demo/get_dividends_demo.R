1f751c2acfa1809544ab95d7a81ae2e93ac984c0
DEVELOPER\deanr <deanr3@bardstown.com>
2023-10-06 06:03:05 -0400

    wrong library in demo

--- demo/get_dividends_demo.R
+++ demo/get_dividends_demo.R
@@ -6,14 +6,14 @@ library(RstocksPkg)
 
 # call get_dividends() with all the argument defaults to get a data.frame of IBM dividends
 #
-ibm_dividends_lst <- RstocksPkg::get_dividends()
-str(ibm_dividends_lst$data)
+ibm_dividends_dt <- RstocksPkg::get_dividends()
+head(ibm_dividends_lst)
 
 # call get_dividends() with 2 stocks, a time frame, and pad the Date column
-ibm_apple_dividends_lst <- RstocksPkg::get_dividends(
+ibm_apple_dividends_dt <- RstocksPkg::get_dividends(
   symbols = c("IBM","AAPL"),
   from = "2000-01-01",
   to = "2022-12-31",
   pad_by_month = TRUE
 )
-str(ibm_apple_dividends_lst$data)
+head(ibm_apple_dividends_dt)
